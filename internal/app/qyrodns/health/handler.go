9071f04a81d4ecb9669e2425decbe851e7e609b0
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-08-29 13:02:58 +0530

    added index route

--- internal/app/qyrodns/health/handler.go
+++ internal/app/qyrodns/health/handler.go
@@ -18,6 +18,10 @@ func NewCheckHandler(router *gin.Engine) *CheckHandler {
 
 func (h *CheckHandler) Register() {
 
+	h.router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, &ServiceInfoResponse{Service: "qyrodns"})
+	})
+
 	h.router.GET("/health", func(c *gin.Context) {
 		c.JSONP(http.StatusOK, &CheckResponse{Status: "ok"})
 	})
--- internal/app/qyrodns/health/responses.go
+++ internal/app/qyrodns/health/responses.go
@@ -3,3 +3,7 @@ package health
 type CheckResponse struct {
 	Status string `json:"status"`
 }
+
+type ServiceInfoResponse struct {
+	Service string `json:"service"`
+}
